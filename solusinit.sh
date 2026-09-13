3bd600adce97f7251bfad7e7d09774f413bd0100
JeremKy <jeremky@me.com>
2026-09-13 11:38:56 +0200

    Ajout de l’installation des flatpaks

--- solusinit.sh
+++ solusinit.sh
@@ -20,31 +20,53 @@ fi
 # Fonctions
 install_packages() {
   warning "Mise à jour des paquets"
-  eopkg -y upgrade || { error "Problème lors de la mise à jour des paquets"; }
+  eopkg -y upgrade || {
+    error "Problème lors de la mise à jour des paquets"
+    return 1
+  }
   echo
   if [[ -f "$list" ]]; then
     warning "Installation des paquets"
-    grep -v -e '#' -e '^$' "$list" | xargs eopkg -y install || {
+    grep -v -e '#' -e '^$' "$list" | xargs -r eopkg -y install || {
       error "Problème lors de l'installation des paquets"
+      return 1
     }
     message "Installation des paquets terminée"
     echo
   fi
 }
 
+install_flatpaks() {
+  if [[ -f "$apps" ]]; then
+    warning "Installation des flatpaks"
+    grep -v -e '#' -e '^$' "$apps" | xargs -r flatpak install -y --system flathub || {
+      error "Problème lors de l'installation des flatpaks"
+      return 1
+    }
+    message "Installation des flatpaks terminée"
+    echo
+  fi
+}
+
 install_geforcenow() {
   warning "Installation de GeforceNow"
-  sudo flatpak remote-add --system --if-not-exists GeForceNOW https://international.download.nvidia.com/GFNLinux/flatpak/geforcenow.flatpakrepo
-  sudo flatpak install flathub org.freedesktop.Platform/x86_64/24.08
-  sudo flatpak install --system GeForceNOW com.nvidia.geforcenow
+  flatpak remote-add --system --if-not-exists GeForceNOW https://international.download.nvidia.com/GFNLinux/flatpak/geforcenow.flatpakrepo
+  flatpak install flathub org.freedesktop.Platform/x86_64/24.08
+  flatpak install -y --system GeForceNOW com.nvidia.geforcenow
   message "Installation de GeforceNow terminée"
   echo
 }
 
 install_hytale() {
   warning "Installation de Hytale"
-  (cd /tmp && wget https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak)
-  flatpak --system install /tmp/hytale-launcher-latest.flatpak
+  (cd /tmp && wget https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak) || {
+    error "Problème lors du téléchargement de Hytale"
+    return 1
+  }
+  flatpak --system install -y /tmp/hytale-launcher-latest.flatpak || {
+    error "Problème lors de l'installation de Hytale"
+    return 1
+  }
   message "Installation de Hytale terminée"
   echo
 }
@@ -53,6 +75,7 @@ install_hytale() {
 dir="$(dirname "$0")/config"
 cfg="$dir/config.cfg"
 list="$dir/packages.cfg"
+apps="$dir/flatpaks.cfg"
 if [[ ! -f "$cfg" ]] || [[ ! -f "$list" ]]; then
   error "Fichier $cfg ou $list introuvable"
   exit 1
